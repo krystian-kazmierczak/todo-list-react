41348aa99fb317c60e8a9f573d5e60b413fb16ce
Krystian Kaźmierczak <s155774@gmail.com>
2021-07-14 21:47:52 +0200

    added theme & GlobalStyled

---
+++ src/GlobalStyle.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+import { normalize } from "styled-normalize";
+
+export const GlobalStyle = createGlobalStyle`
+  ${normalize}
+  
+  html {
+    box-sizing: border-box;
+  }
+  *, ::after, ::before {
+    box-sizing: inherit;
+  }
+  body {
+    font-family: 'Montserrat', sans-serif;
+    background-color: ${({ theme }) => theme.colors.gallery};
+  }
+`;
---
+++ src/theme.js
@@ -0,0 +1,15 @@
+export const theme = {
+    colors: {
+        teal: "hsl(180, 100%, 25%)",
+        gallery: "hsl(0, 0%, 93%)",
+        alto: "hsl(0, 0%, 87%)",
+        silver: "hsl(0, 0%, 80%)",
+        white: "hsl(0, 0%, 100%)",
+        red: "hsl(349, 86%, 50%)",
+        green: "hsl(120, 100%, 25%)",
+
+    },
+    breakpoints: {
+        mobile: 767,
+    }
+};
