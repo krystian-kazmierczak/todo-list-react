cd413a46bbb123b1ea80813b12df20811e26e4e5
Krystian Kaźmierczak <s155774@gmail.com>
2021-07-14 21:58:58 +0200

    added styled-components in Container

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Tasks from "./Tasks";
 import Buttons from "./Buttons";
 import Section from "./Section";
 import Header from "./Header";
-import Container from "./Container";
+import Container from "./Container/styled";
 
 function App() {
   const [hideDoneTasks, setHideDoneTasks] = useState(false);
